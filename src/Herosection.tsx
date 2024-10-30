609bc4900eb571d02375148712ff9bd7e0590d0b
24164003 <24164003@studentmail.ul.ie>
2024-10-30 11:45:35 +0530

    first commit

---
+++ src/Herosection.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Herosection = () => {
+    return (
+        <div className='herosection-parent'>
+            <div className='herosection-text teko-bold'>SM</div>
+            <div className='herosection-tagline'>"DESIGNER DEVOTED TO CREATING MEMORABLE EXPERIENCES"</div>
+            <div style={{color: "white", fontSize: "13px", marginTop: '25px'}}>© 2024 Shonal Maria. All Rights Reserved.</div>
+        </div>
+    )
+}
+
+export default Herosection
---
+++ src/Portfolio.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import portfolioPDF from "../public/pdfportfolio.pdf"
+
+const Portfolio = () => {
+    return (
+        <div><iframe src={portfolioPDF} style={{ height: '92vh', width: '100%', overflowY: 'hidden' }}></iframe></div>
+    )
+}
+
+export default Portfolio
